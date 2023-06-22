d7175decaaa7bbddb6f96c831eed4b0ad4c17414
Naji-97 <elbairabennaji@gmail.com>
2023-06-22 16:03:23 +0100

    add fundamental user interface

---
+++ app.rb
@@ -0,0 +1,176 @@
+require_relative 'person'
+require_relative 'classroom'
+require_relative 'student'
+require_relative 'teacher'
+require_relative 'book'
+require_relative 'rental'
+
+class App
+  def initialize
+    @people = []
+    @books = []
+    @rentals = []
+  end
+
+  def display_menu
+    puts 'Welcome to the School Library App'
+    puts 'Please choose an option by entering a number:'
+    puts '1. List all books'
+    puts '2. List all people'
+    puts '3. Create a person'
+    puts '4. Create a book'
+    puts '5. Create a rental'
+    puts '6. List all rentals for a given person id'
+    puts '7. Exit'
+  end
+
+  def list_books
+    puts 'List of Books:'
+    @books.each { |book| puts "#{book.title} by #{book.author}" }
+  end
+
+  def list_people
+    puts 'List of People:'
+    @people.each do |person|
+      if person.is_a?(Teacher)
+        puts "[Teacher] Name: #{person.name} ID: #{person.id}: Age: #{person.age}"
+      elsif person.is_a?(Student)
+        puts "[Student] Name: #{person.name} ID: #{person.id}: Age: #{person.age}"
+      else
+        puts "Person Name: #{person.name} ID: #{person.id}: Age: #{person.age}"
+      end
+    end
+  end
+
+  def create_person
+    puts 'Create a Person'
+    puts 'Do you want to create a student (1) or a teacher (2)? [Input the number]:'
+    person_type = gets.chomp.to_i
+
+    case person_type
+    when 1
+      create_student
+    when 2
+      create_teacher
+    else
+      puts 'Invalid person type'
+    end
+  end
+
+  def create_student
+    puts 'Create a Student'
+    puts 'Age:'
+    age = gets.chomp.to_i
+    puts 'Name:'
+    name = gets.chomp
+    puts 'Has parent permission? [Y/N]:'
+    parent_permission = gets.chomp.downcase == 'y'
+
+    student = Student.new(name, age, parent_permission: parent_permission)
+    @people << student
+    puts 'Student created successfully'
+  end
+
+  def create_teacher
+    puts 'Create a Teacher'
+    puts 'Age:'
+    age = gets.chomp.to_i
+    puts 'Name:'
+    name = gets.chomp
+    puts 'Specialization:'
+    specialization = gets.chomp
+
+    teacher = Teacher.new(name, age, parent_permission: true, specialization: specialization)
+    @people << teacher
+    puts 'Teacher created successfully'
+  end
+
+  def create_book
+    puts 'Create a Book'
+    puts 'Title:'
+    title = gets.chomp
+    puts 'Author:'
+    author = gets.chomp
+
+    book = Book.new(title, author)
+    @books << book
+    puts 'Book created successfully'
+  end
+
+  def create_rental
+    puts 'Create a Rental'
+    puts 'Enter person ID:'
+    person_id = gets.chomp.to_i
+
+    person = @people.find { |p| p.id == person_id }
+    unless person
+      puts 'Invalid person ID'
+      return
+    end
+
+    puts 'Enter book title:'
+    book_title = gets.chomp
+
+    book = @books.find { |b| b.title == book_title }
+    unless book
+      puts 'Invalid book title'
+      return
+    end
+
+    puts 'Enter rental date (YYYY-MM-DD):'
+    date = gets.chomp
+
+    rental = Rental.new(date, book, person)
+    @rentals << rental
+    puts 'Rental created successfully'
+  end
+
+  def list_rentals_for_person
+    puts 'List all rentals for a given person id'
+    puts 'Enter person ID:'
+    person_id = gets.chomp.to_i
+
+    person = @people.find { |p| p.id == person_id }
+    unless person
+      puts 'Invalid person ID'
+      return
+    end
+
+    rentals = @rentals.select { |r| r.person == person }
+    if rentals.empty?
+      puts "No rentals found for #{person.name}"
+    else
+      puts "Rentals for #{person.name}:"
+      rentals.each { |rental| puts "#{rental.book.title}, Date: #{rental.date}" }
+    end
+  end
+
+  def run
+    loop do
+      display_menu
+
+      option = gets.chomp.to_i
+
+      case option
+      when 1
+        list_books
+      when 2
+        list_people
+      when 3
+        create_person
+      when 4
+        create_book
+      when 5
+        create_rental
+      when 6
+        list_rentals_for_person
+      when 7
+        break
+      else
+        puts 'Invalid option'
+      end
+
+      puts "\n"
+    end
+  end
+end
--- main.rb
+++ main.rb
@@ -1,33 +1,41 @@
-require_relative 'person'
-require_relative 'classroom'
-require_relative 'student'
-require_relative 'teacher'
-require_relative 'decorator'
-require_relative 'book'
-require_relative 'rental'
+# require_relative 'person'
+# require_relative 'classroom'
+# require_relative 'student'
+# require_relative 'teacher'
+# require_relative 'decorator'
+# require_relative 'book'
+# require_relative 'rental'
 
-# Create a Classroom object
-classroom = Classroom.new('Class A')
+# # Create a Classroom object
+# classroom = Classroom.new('Class A')
 
-# Create Student objects
-Student.new('John Doe', 16, classroom: classroom)
-Student.new('Jane Smith', 17, classroom: classroom)
+# # Create Student objects
+# Student.new('John Doe', 16, classroom: classroom)
+# Student.new('Jane Smith', 17, classroom: classroom)
 
-# Print the classroom label and its students
-puts "Classroom: #{classroom.label}"
-puts "Students in the classroom: #{classroom.students.map(&:name).join(', ')}"
+# # Print the classroom label and its students
+# puts "Classroom: #{classroom.label}"
+# puts "Students in the classroom: #{classroom.students.map(&:name).join(', ')}"
 
-# Create a Book object
-book = Book.new('The Great Gatsby', 'F. Scott Fitzgerald')
+# # Create a Book object
+# book = Book.new('The Great Gatsby', 'F. Scott Fitzgerald')
 
-# Create Person objects
-person1 = Person.new('Alice')
-person2 = Person.new('Bob')
+# # Create Person objects
+# person1 = Person.new('Alice')
+# person2 = Person.new('Bob')
 
-# Create Rental objects
-Rental.new('2023-06-20', book, person1)
-Rental.new('2023-06-21', book, person2)
+# # Create Rental objects
+# Rental.new('2023-06-20', book, person1)
+# Rental.new('2023-06-21', book, person2)
 
-# Print the book title and its rentals
-puts "Book: #{book.title}"
-puts "Rentals for the book: #{book.rentals.map(&:person).map(&:name).join(', ')}"
+# # Print the book title and its rentals
+# puts "Book: #{book.title}"
+# puts "Rentals for the book: #{book.rentals.map(&:person).map(&:name).join(', ')}"
+
+require_relative 'app'
+
+def main
+  App.new.run
+end
+
+main
