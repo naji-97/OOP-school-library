5180df56ab78c0b3387be9ef86baf7d6db1d38f7
Naji-97 <elbairabennaji@gmail.com>
2023-06-26 16:54:58 +0100

    Update main.rb

--- main.rb
+++ main.rb
@@ -8,30 +8,3 @@ loop do
   option_processor.process_option(option)
   puts "\n"
 end
-
-class Menu
-  def initialize(app)
-    @app = app
-  end
-
-  def process_option(option)
-    option_actions = {
-      1 => :list_books,
-      2 => :list_people,
-      3 => :create_person,
-      4 => :create_book,
-      5 => :create_rental,
-      6 => :list_rentals_for_person,
-      7 => :exit_app
-    }
-    if option_actions.key?(option)
-      @app.send(option_actions[option])
-    else
-      puts 'Invalid option'
-    end
-  end
-  # def exit_app7
-  #   puts 'Exiting the app...'
-  #   exit
-  # end
-end
